cd0016891fd15174a95e5a0cdfbe9ced8201cc8e
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-06-27 08:28:43 +0700

    Update PY02065. DIEN TICH TOAN PHAN.py

--- CHỦ ĐỀ 2. DANH SÁCH/PY02065. DIEN TICH TOAN PHAN.py
+++ CHỦ ĐỀ 2. DANH SÁCH/PY02065. DIEN TICH TOAN PHAN.py
@@ -1,26 +1,30 @@
 #Diện tích toàn phần của cột có chiều cao h nằm trong 1 ô vuông đơn vị
 def area(h):
-    if h==0: return 0
-    return h*4+2
-for t in range(int(input())):
-    n, m = map(int, input().split())
-    ans = 0
-    arr = []
+    if h == 0: return 0
+    return h * 4 + 2
 
-    #Phần diện tích xung quanh chung giữa cột có chiều cao a[i][j] và cột có chiều cao h
-    #Chỉ xét chiều đi tức là một cột đang xét thì ta chỉ xét cột bên trái, và bên trên nó
-    def inter(i, j, h):
-        if i==0 and j==0: return 0
-        #Hàng đầu thì chỉ bị che bởi cột bên trái
-        if i==0: return min(h,arr[i][j-1])
-        #Cột đầu thì chỉ bị che bởi cột phía trên
-        if j==0: return min(h,arr[i-1][j])
-        return min(h,arr[i][j-1]) + min(h,arr[i-1][j])
-    
+# Phần diện tích xung quanh chung giữa cột có chiều cao a[i][j] và cột có chiều cao h
+# Chỉ xét chiều đi tức là một cột đang xét thì ta chỉ xét cột bên trái, và bên trên nó
+def inter(i, j, h, arr):
+    if i == 0 and j == 0: return 0
+    # Hàng đầu thì chỉ bị che bởi cột bên trái
+    if i == 0: return min(h, arr[i][j-1])
+    # Cột đầu thì chỉ bị che bởi cột phía trên
+    if j == 0: return min(h, arr[i-1][j])
+    return min(h, arr[i][j-1]) + min(h, arr[i-1][j])
+
+def calc(n, m, arr):
+    ans = 0
+    for i in range(n):
+        for j in range(m):
+            s = inter(i, j, arr[i][j], arr)
+            ans += area(arr[i][j]) - s * 2
+    return ans
+
+for _ in range(int(input())):
+    n, m = map(int, input().split())
+    arr = []
     for i in range(n):
         a = list(map(int, input().split()))
         arr.append(a)
-        for j in range(m):
-            s = inter(i, j, a[j])
-            ans+=area(a[j])-s*2
-    print(ans)
+    print(calc(n, m, arr))
