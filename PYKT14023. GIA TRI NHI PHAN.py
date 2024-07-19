a18582754537ba66503ef1e59634f3968b8dc98a
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-07-19 10:05:24 +0700

    Create PYKT14023. GIA TRI NHI PHAN.py

---
+++ PYKT14023. GIA TRI NHI PHAN.py
@@ -0,0 +1,9 @@
+n, q = map(int, input().split())
+a = [0]*(n+2)
+#Ở đây do mảng gốc là toàn 0 nên a chính là mảng hiệu luôn
+for i in range(q):
+    x, y = map(int, input().split())
+    a[x]+=1
+    a[y+1]-=1
+for i in range(1,n+1): a[i]+=a[i-1]
+for i in range(1,n+1): print(a[i]%2, end=' ')
